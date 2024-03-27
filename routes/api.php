c2943d5388d0b22605405688857f0d369a1cfffc
ErickSiguache <erickaguilars403@gmail.com>
2024-03-27 16:37:40 -0600

    feat: add the category controller paths to make visible the endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/category", [CategoryController::class, 'getAll']);
+Route::get('/category/{id}', [CategoryController::class, 'get']);
+Route::post('/category', [CategoryController::class, 'create']);
+Route::put('/category/{id}', [CategoryController::class, 'update']);
+Route::delete('/category/{id}', [CategoryController::class, 'delete']);
